fefe1be6a2292d14da054138f12f8787b54b3c87
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-20 13:45:17 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+cont = int(1)
+while cont <= 9:
+    j = int(cont)
+    print("I={} J={}".format(cont, j + 6))
+    print("I={} J={}".format(cont, j + 5))
+    print("I={} J={}".format(cont, j + 4))
+    cont = cont + 2
